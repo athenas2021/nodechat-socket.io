916e832b7e4d0fe7cd0fdce53869fd7e1d26bc58
athenas2021 <lucas.kano@outlook.com>
2021-08-13 17:03:06 -0300

    algum bug!

--- public/js/main.js
+++ public/js/main.js
@@ -1,35 +1,83 @@
 const chatBox = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+//pega nome do usuario da URL
+const {username,room} = Qs.parse(location.search,{
+    ignoreQueryPrefix: true
+});
 
 const socket = io();
 
-socket.on('welcome_message',message =>{
-    console.log('Mensagem de boas-vindas:'+message)
+//evento quando entra na sala
+socket.emit('joinRoom', {username,room});
+
+//Pegar nome da sala e usuarios
+socket.on('roomUsers',({room,users}) => {
+    outputRoomName(room);
+    outputUsers(users);
+});
+
+//socket.on('welcome_message',message =>{
+    //console.log('Mensagem de boas-vindas:'+message)
+  //  outputMessage(message);
+//});
+
+
+socket.on('message',message =>{    
     outputMessage(message);
-})
+    // Scroll down
+chatMessages.scrollTop = chatMessages.scrollHeight;
 
+});
 
-socket.on('message',message =>{
-    console.log('Mensagem :'+message)
-    outputMessage(message);
-})
-
-socket.on('chatMessage',message =>{
+ //socket.on('chatMessage',message =>{
     //console.log('Mensagem :'+message)
-    outputMessage(message);
-})
+   //  outputMessage(message);
+ //} );
+
 
 chatBox.addEventListener('submit', (e) => {
     e.preventDefault();
-    const msg = e.target.elements.msg.value;
+    let msg = e.target.elements.msg.value;
+    msg = msg.trim();
+    if (!msg) {
+        return false;
+      }
     socket.emit('chatMessage',msg);
-})
+    // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
 
 function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML =  `<p class="meta">Lucas <span>9:12pm</span></p>
-    <p class="text">
-        ${message}
-    </p>`;
+    const p = document.createElement('p');
+    p.classList.add('meta'); p.innerText = message.username;
+    p.innerHTML += `<span>${message.time}</span>`;
+    div.appendChild(p);
+    const para = document.createElement('p');
+    para.classList.add('text');
+    para.innerText = message.text;
+    div.appendChild(para);
     document.querySelector('.chat-messages').appendChild(div);
+  
+}
+
+//adicionar nome da sala atual ao HTML DOM
+function outputRoomName(room){
+    roomName.innerText = room;     
+}
+
+//Adcionar usuarios atuais ao HTML DOM
+
+function outputUsers(users){
+    userList.innerHTML = '';
+    users.forEach((user) => {
+      const li = document.createElement('li');
+      li.innerText = user.username;
+      userList.appendChild(li);
+    });
 }
---
+++ utils/users.js
@@ -0,0 +1,34 @@
+const users = [];
+
+//Quando usuario entar na sala
+function userJoin(id,username,room){
+    const user = {id,username,room};
+    users.push(user);
+    return user;
+}
+
+//pegar usuario atual
+
+function getCurrentUser(id){
+    return users.find(user => user.id == id);
+}
+
+//usuario sai da sala
+function userLeave(id){
+    const index = users.findIndex(user => user.id == id);
+    if(index !== -1 ){
+        return users.splice(index,1)[0];
+    }
+}
+
+//pega lista de usuarios
+function getRoomUsers(room){
+    return users.filter(user=>user.room=== room);
+}
+
+module.exports = {
+    userJoin,
+    getCurrentUser,
+    userLeave,
+    getRoomUsers
+};
